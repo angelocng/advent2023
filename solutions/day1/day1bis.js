ef6bae9edc7c4879fefd7167fad3bda0fb3d0fe7
Angelo Congiu <angelocongiu@MacBook-Air-de-Angelo.local>
2023-12-17 12:39:14 +0100

    day1bis

--- solutions/day1/day1bis.js
+++ solutions/day1/day1bis.js
@@ -29,35 +29,47 @@ function isInt(value) {
     return (x | 0) === x;
   }
 
+function toNumber(string){
+    let dict={'one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
+    if (string in dict){
+        return dict[string]
+    }
+    return string
+}
+
 function findCalibration(data){
-    
+    const regex = /(?=(([0-9])|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)))/g
     let tab=data.split(/\n/)
     let pass=[]
+    let match=[]
     tab.forEach(element => {
-        console.log(element)
         if (element != "")
         {
-            let i = 0;
-            while (!isInt(element[i])){
-                ++i
-            }
-            pass.push(element[i])
+            match = [...element.matchAll(regex)].map(match => match[1])
 
-            i = element.length-1
-            while (!isInt(element[i])){
-                i--
-            }
+            pass.push(toNumber(match[0]))
 
-            pass[pass.length-1]+= element[i]
+
+            if (match.length==1){
+                pass[pass.length-1]+= toNumber(match[0])
+                
+            }
+            else{
+                pass[pass.length-1]+= toNumber(match[match.length-1])
+            }
         }
+        
     });
+
     return pass
 }
 
 function findSum(pass){
     let sum = 0
     pass.forEach(cal=>{
+        console.log(cal)
         sum+=+cal
+        console.log(sum)
     })
     return sum
 }
